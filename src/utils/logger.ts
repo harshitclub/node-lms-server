f89295c61f24233b5de5034b4ed91f8303cebc2b
harshitclub <harshitclub@gmail.com>
2025-01-06 16:08:22 +0530

    feat: Adding source map

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,8 +1,13 @@
 import util from 'util'
 import { createLogger, format, transports } from 'winston'
-import { ConsoleTransportInstance } from 'winston/lib/winston/transports'
+import { ConsoleTransportInstance, FileTransportInstance } from 'winston/lib/winston/transports'
 import config from '../configs/config'
 import { EApplicationEnvironment } from '../constants/application'
+import path from 'path'
+import * as sourceMapSupport from 'source-map-support'
+
+// Linking Trace Support
+sourceMapSupport.install()
 
 const consoleLogFormat = format.printf((info) => {
     const { level, message, timestamp, meta = {} } = info
@@ -37,9 +42,55 @@ const consoleTransport = (): Array<ConsoleTransportInstance> => {
     return []
 }
 
+const fileLogFormat = format.printf((info) => {
+     
+    const { level, message, timestamp, meta = {} } = info
+
+    const logMeta: Record<string, unknown> = {}
+
+    // Type guard to ensure meta is an object
+    if (typeof meta === 'object' && meta !== null) {
+        for (const [key, value] of Object.entries(meta)) {
+            if (value instanceof Error) {
+                logMeta[key] = {
+                    name: value.name,
+                    message: value.message,
+                    trace: value.stack || ''
+                }
+            } else {
+                logMeta[key] = value
+            }
+        }
+    } else {
+        // Handle the case where meta is not an object (optional)
+        logMeta['meta_was_not_an_object'] = meta
+    }
+
+    const logData = {
+        level: level.toUpperCase(),
+         
+        message,
+         
+        timestamp,
+        meta: logMeta
+    }
+
+    return JSON.stringify(logData, null, 4)
+})
+
+const FileTransport = (): Array<FileTransportInstance> => {
+    return [
+        new transports.File({
+            filename: path.join(__dirname, '../', '../', 'logs', `${config.ENV}.log`),
+            level: 'info',
+            format: format.combine(format.timestamp(), fileLogFormat)
+        })
+    ]
+}
+
 export default createLogger({
     defaultMeta: {
         meta: {}
     },
-    transports: [...consoleTransport()]
+    transports: [...FileTransport(), ...consoleTransport()]
 })
