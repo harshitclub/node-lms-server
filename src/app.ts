218d3f1c4a48a5e5fe1681418c46eaa59bf3a0df
harshitclub <harshitclub@gmail.com>
2025-01-08 11:32:21 +0530

    feat: Adding rate limiter

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application, NextFunction, Request, Response } from 'express'
 import helmet from 'helmet'
 import cors from 'cors'
+import rateLimit from 'express-rate-limit'
 import router from './routes/apiRoutes'
 import globalErrorHandler from './middlewares/globalErrorHandler'
 import responseMessage from './constants/responseMessage'
@@ -21,12 +22,24 @@ app.use(
     })
 )
 app.use(express.json())
+// Rate Limiting Configuration (Replace with your desired settings)
+const rateLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes window
+    max: 100, // Allow up to 100 requests per IP within the window
+    message: {
+        code: 'TOO_MANY_REQUESTS',
+        message: 'Too many requests from this IP, please try again later.'
+    } // Customize the error message
+})
+
+// Apply rate limiting to all routes
+app.use(rateLimiter)
 
 // Routes
-app.use('/api/v1', router)
-app.use('/api/v1/admin', adminRouter)
-app.use('/api/v1/company', companyRouter)
-app.use('/api/v1/employee', employeeRouter)
+app.use('/api/v1', router) //testing router
+app.use('/api/v1/admin', adminRouter) // admin router
+app.use('/api/v1/company', companyRouter) // company router
+app.use('/api/v1/employee', employeeRouter) // employee router
 
 // 404 Handler
 app.use((req: Request, _: Response, next: NextFunction) => {
